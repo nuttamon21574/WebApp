4ee2c8f02e4bacbc2ef0e84c760e9b782d430a5d
merxg <g.chamnankan@gmail.com>
2026-01-13 22:16:03 +0700

    change Sidebar

--- Fontend/src/components/Sidebar/Sidebar.jsx
+++ Fontend/src/components/Sidebar/Sidebar.jsx
@@ -8,39 +8,6 @@ export default function Sidebar() {
 
   const menuItems = [
     {
-      name: "My Account",
-      path: "/my-account",
-      icon: (
-        <svg className="w-5 h-5 me-2" fill="#2B1175" viewBox="0 0 24 24">
-          <path
-            fillRule="evenodd"
-            d="M12 20a7.966 7.966 0 0 1-5.002-1.756v-.683c0-1.794 1.492-3.25 3.333-3.25h3.334c1.84 0 3.333 1.456 3.333 3.25v.683A7.966 7.966 0 0 1 12 20ZM2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.44 9.963-9.932 10h-.136C6.438 21.963 2 17.5 2 12Zm10-5c-1.84 0-3.333 1.455-3.333 3.25S10.159 13.5 12 13.5c1.84 0 3.333-1.455 3.333-3.25S13.841 7 12 7Z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-    },
-    {
-      name: "BNPL Debt",
-      path: "/bnpl",
-      icon: (
-        <svg className="w-5 h-5 me-2" fill="#2B1175" viewBox="0 0 18 18">
-          <path d="M12 14a3 3 0 0 1 3-3h4a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2h-4a3 3 0 0 1-3-3Z" />
-          <path d="M4.586 7A2 2 0 0 0 3 9v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2h-4a5 5 0 0 1 0-10h4" />
-        </svg>
-      ),
-    },
-    {
-      name: "Dashboard",
-      path: "/dashboard",
-      icon: (
-        <svg className="w-5 h-5 me-2" fill="#2B1175" viewBox="0 0 24 24">
-          <path d="M13.5 2a8.5 8.5 0 0 1 8.5 8.5h-9V3a1 1 0 0 0-1-1Z" />
-          <path d="M11 6a1 1 0 0 0-1-1A8.5 8.5 0 1 0 18 14h-7V6Z" />
-        </svg>
-      ),
-    },
-    {
       name: "Recommendations",
       path: "/Recommendations",
       icon: (
@@ -53,6 +20,43 @@ export default function Sidebar() {
         </svg>
       ),
     },
+
+        {
+      name: "Dashboard",
+      path: "/dashboard",
+      icon: (
+        <svg className="w-5 h-5 me-2" fill="#2B1175" viewBox="0 0 24 24">
+          <path d="M13.5 2a8.5 8.5 0 0 1 8.5 8.5h-9V3a1 1 0 0 0-1-1Z" />
+          <path d="M11 6a1 1 0 0 0-1-1A8.5 8.5 0 1 0 18 14h-7V6Z" />
+        </svg>
+      ),
+    },
+
+    {
+      name: "Buy Now Pay Later Debt",
+      path: "/bnpl",
+      icon: (
+        <svg className="w-5 h-5 me-2" fill="#2B1175" viewBox="0 0 18 18">
+          <path d="M12 14a3 3 0 0 1 3-3h4a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2h-4a3 3 0 0 1-3-3Z" />
+          <path d="M4.586 7A2 2 0 0 0 3 9v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2h-4a5 5 0 0 1 0-10h4" />
+        </svg>
+      ),
+    },
+
+    {
+      name: "My Account",
+      path: "/my-account",
+      icon: (
+        <svg className="w-5 h-5 me-2" fill="#2B1175" viewBox="0 0 24 24">
+          <path
+            fillRule="evenodd"
+            d="M12 20a7.966 7.966 0 0 1-5.002-1.756v-.683c0-1.794 1.492-3.25 3.333-3.25h3.334c1.84 0 3.333 1.456 3.333 3.25v.683A7.966 7.966 0 0 1 12 20ZM2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.44 9.963-9.932 10h-.136C6.438 21.963 2 17.5 2 12Zm10-5c-1.84 0-3.333 1.455-3.333 3.25S10.159 13.5 12 13.5c1.84 0 3.333-1.455 3.333-3.25S13.841 7 12 7Z"
+            clipRule="evenodd"
+          />
+        </svg>
+      ),
+    },
+    
   ];
 
   const logoutItem = {
