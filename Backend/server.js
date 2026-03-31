b510da42af687df966b9b1c05b02feec30aa3656
nuttamon21574 <nuttamon21574@gmail.com>
2026-03-31 20:10:19 +0700

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -27,7 +27,7 @@ console.log("Using Production Firebase Credentials");
 credential = admin.credential.cert({
 projectId: process.env.FB_PROJECT_ID,
 clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\n/g, "\n"),
+privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
 });
 
 } else {
